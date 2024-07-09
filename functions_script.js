42185726c100ef2119adb3e46b1b1b1a316bbd02
Bastos2894 <135364540+Bastos2894@users.noreply.github.com>
2024-07-09 13:51:51 -0400

    primeiro commit

---
+++ functions_script.js
@@ -0,0 +1,74 @@
+async function getAddresByCep() {
+    const cep = document.getElementById("cep").value;
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+        console.log(data);
+        document.getElementById('rua').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('cidade').value = data.localidade;
+        console.log(response);
+    } catch (error) {
+        alert("Dados incompletos");
+    }
+}
+
+
+async function getPrevisao() {
+    const latInput = document.getElementById("lat");
+    const longInput = document.getElementById("log");
+
+    if (!latInput || !longInput) {
+        alert("Campos de entrada não encontrados.");
+        return;
+    }
+
+    const lat = latInput.value;
+    const long = longInput.value;
+
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`);
+
+        if (!response.ok) {
+            throw new Error('Erro ao carregar os dados da API.');
+        }
+
+        const data = await response.json();
+
+        if (!data.hourly || !data.hourly.temperature_2m || data.hourly.temperature_2m.length === 0) {
+            throw new Error('Dados de temperatura não encontrados.');
+        }
+        document.getElementById('temp').innerHTML = `Previsão do tempo de acordo com a região: ${data.hourly.temperature_2m[0]}º C`;
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+
+function btn() {
+    getAddresByCep();
+    getPrevisao();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
